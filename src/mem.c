bf049d30b218a7de5a0d65cb3c4bb33b7a7aa10d
huydzttvv <90630869+huydzttvv@users.noreply.github.com>
2021-10-27 21:56:58 +0700

    Update mem.c

--- src/mem.c
+++ src/mem.c
@@ -53,6 +53,8 @@ static struct page_table_t * get_page_table(
 	int i;
 	for (i = 0; i < seg_table->size; i++) {
 		// Enter your code here
+		if( seg_table −> table[i].v_index == index )
+			return seg_table −> table[i].pages ;
 	}
 	return NULL;
 
@@ -87,6 +89,8 @@ static int translate(
 			 * to [p_index] field of page_table->table[i] to 
 			 * produce the correct physical address and save it to
 			 * [*physical_addr]  */
+			// addr_t p_index = page_table ->table[i].p_index; //physical page index
+			∗ physical_addr = ( page_table −> table[i].p_index << OFFSET_LEN) + offset;
 			return 1;
 		}
 	}
